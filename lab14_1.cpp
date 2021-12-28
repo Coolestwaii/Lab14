0e7d5bb16e699d409fec7af4702aed3437e2521d
waii <yougle55567@gmail.com>
2021-12-28 21:41:55 +0700

    lab14 done

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,23 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+    double sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+    double max = a[0], min = a[0];
+    for(int i = 0; i < n; i++){
+        if(a[i] > max) max = a[i];
+        if(a[i] < min) min = a[i];
+
+        sum1 += a[i];
+        sum2 += pow(a[i], 2);
+        sum3 += log(a[i]);
+        sum4 += 1/(a[i]);
+    }
+    b[0] = sum1/n;
+    b[1] = sqrt((sum2/n) - pow(b[0],2));
+    b[2] = exp(sum3/n);
+    b[3] = n/sum4;
+    b[4] = max;
+    b[5] = min;
+}
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,28 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[][M], int a, int b, int c){
+    double n;
+    for(int x = 0; x < N; x++){
+        for(int y = 0; y < M; y++){
+            n = sqrt(pow((x - b), 2) + pow((y - c), 2)) + 1;
+            if(n <= a){
+                image[x][y] = 1;
+            }
+        }
+    }
+}
+
+void showImage(const bool image[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int x = 0; x < N; x++){
+        cout << "|";
+        for(int y = 0; y < M; y++){
+            if(image[x][y] == 0) cout << " ";
+            if(image[x][y] == 1) cout << "*";
+        }
+    cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
